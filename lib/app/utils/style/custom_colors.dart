e76f068d7d3e068314785268e7ec6df21ea205de
tribber93 <tribberyoni5@gmail.com>
2022-07-02 03:09:02 +0700

    tambahan dari video ke-5

---
+++ lib/app/utils/style/custom_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const primaryBg = Color(0xFFBBDEFB);
+  static const primaryText = Color.fromARGB(255, 113, 113, 113);
+}
--- lib/app/utils/widget/header.dart
+++ lib/app/utils/widget/header.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
+import '../style/custom_colors.dart';
 
 import '../../routes/app_pages.dart';
 
@@ -15,6 +16,7 @@ class Header extends StatelessWidget {
     return SizedBox(
       height: Get.height * 0.1,
       child: Padding(
+        //
         padding: const EdgeInsets.only(left: 15, right: 30, top: 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -24,36 +26,44 @@ class Header extends StatelessWidget {
               children: const [
                 Text(
                   'Task Management',
-                  style: TextStyle(fontSize: 20, color: Colors.grey),
+                  style:
+                      TextStyle(fontSize: 20, color: CustomColor.primaryText),
                 ),
                 Text('Menjadi mudah dengan Task Management',
-                    style: TextStyle(fontSize: 15, color: Colors.grey))
+                    style:
+                        TextStyle(fontSize: 15, color: CustomColor.primaryText))
               ],
             ),
             const Spacer(
               flex: 1,
             ),
             Expanded(
-                child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.only(left: 40, right: 10),
-                enabledBorder: OutlineInputBorder(
+              flex: 1,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.only(left: 40, right: 10),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(color: Colors.white)),
+                  focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
-                    borderSide: const BorderSide(color: Colors.white)),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: const BorderSide(color: Colors.blue),
+                    borderSide: const BorderSide(color: Colors.blue),
+                  ),
+                  prefixIcon: const Icon(Ionicons.search),
+                  hintText: 'Pencarian',
                 ),
-                prefixIcon: const Icon(Ionicons.search),
-                hintText: 'Pencarian',
               ),
-            )),
+            ),
             const SizedBox(
               width: 20,
             ),
-            const Icon(Ionicons.notifications, color: Colors.grey),
+            const Icon(
+              Ionicons.notifications,
+              color: CustomColor.primaryText,
+              size: 25,
+            ),
             const SizedBox(
               width: 10,
             ),
@@ -74,13 +84,15 @@ class Header extends StatelessWidget {
               child: Row(
                 children: const [
                   Text('Sign out',
-                      style: TextStyle(fontSize: 15, color: Colors.grey)),
+                      style: TextStyle(
+                          fontSize: 18, color: CustomColor.primaryText)),
                   SizedBox(
                     width: 5,
                   ),
                   Icon(
                     Ionicons.log_out_outline,
-                    color: Colors.grey,
+                    color: CustomColor.primaryText,
+                    size: 25,
                   )
                 ],
               ),
