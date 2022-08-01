8b1fe432041d551b48e20fc01e459ff83e65d3a2
tribber93 <tribberyoni5@gmail.com>
2022-08-01 21:16:22 +0700

    video ke-19

--- lib/app/modules/task/controllers/task_controller.dart
+++ lib/app/modules/task/controllers/task_controller.dart
@@ -1,12 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:task_management/app/data/controller/auth_controller.dart';
 
 class TaskController extends GetxController {
   //TODO: Implement TaskController
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  UserCredential? _userCredential;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final authCon = Get.find<AuthController>();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  late TextEditingController titleController,
+      descriptionController,
+      dueDateController;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    titleController = TextEditingController();
+    descriptionController = TextEditingController();
+    dueDateController = TextEditingController();
   }
 
   @override
@@ -17,7 +32,64 @@ class TaskController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    titleController.dispose();
+    descriptionController.dispose();
+    dueDateController.dispose();
   }
 
-  void increment() => count.value++;
+  Future<void> saveUpdateTask(
+      {String? titel,
+      String? description,
+      String? dueDate,
+      String? docId,
+      String? type}) async {
+    print(titel);
+    print(description);
+    print(dueDate);
+    print(docId);
+    print(type);
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    formKey.currentState!.save();
+    CollectionReference taskColl = firestore.collection('task');
+    CollectionReference usersColl = firestore.collection('users');
+    var taskId = DateTime.now().toIso8601String();
+    if (type == 'Add') {
+      await taskColl.doc(taskId).set({
+        'title': titel,
+        'description': description,
+        'dueDate': dueDate,
+        'status': '0',
+        'total_task': '0',
+        'total_task_finished': '0',
+        'task_detail': [],
+        'asign_to': [authCon.auth.currentUser!.email],
+        'created_by': authCon.auth.currentUser!.email,
+      }).whenComplete(() async {
+        await usersColl.doc(authCon.auth.currentUser!.email).set({
+          'task_id': FieldValue.arrayUnion([taskId])
+        }, SetOptions(merge: true));
+        Get.back();
+        Get.snackbar('Task', 'Successfully $type');
+      }).catchError((error) {
+        Get.snackbar('Task', 'Failed to $type');
+      });
+    } else {
+      await taskColl.doc(docId).update({
+        'title': titel,
+        'description': description,
+        'dueDate': dueDate,
+      }).whenComplete(() async {
+        await usersColl.doc(authCon.auth.currentUser!.email).set({
+          'task_id': FieldValue.arrayUnion([taskId])
+        }, SetOptions(merge: true));
+        Get.back();
+        Get.snackbar('Task', 'Successfully $type');
+      }).catchError((error) {
+        Get.snackbar('Task', 'Failed to $type');
+      });
+    }
+  }
 }
--- lib/app/modules/task/views/task_view.dart
+++ lib/app/modules/task/views/task_view.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unnecessary_const
 
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -124,91 +125,100 @@ class TaskView extends GetView<TaskController> {
                               physics: const AlwaysScrollableScrollPhysics(),
                               scrollDirection: Axis.vertical,
                               itemBuilder: (context, index) {
-                                return Container(
-                                  margin: const EdgeInsets.all(10),
-                                  padding: const EdgeInsets.all(15),
-                                  height: 150,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: CustomColor.cardBg,
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(25),
-                                            child: const CircleAvatar(
-                                              backgroundColor: Colors.amber,
-                                              radius: 20,
-                                              foregroundImage: NetworkImage(
-                                                  'https://pbs.twimg.com/profile_images/1488749186610728960/4POimDrS_400x400.jpg'),
-                                            ),
-                                          ),
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(25),
-                                            child: const CircleAvatar(
-                                              backgroundColor: Colors.amber,
-                                              radius: 20,
-                                              foregroundImage: NetworkImage(
-                                                  'https://pbs.twimg.com/profile_images/1488749186610728960/4POimDrS_400x400.jpg'),
-                                            ),
-                                          ),
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(25),
-                                            child: const CircleAvatar(
-                                              backgroundColor: Colors.amber,
-                                              radius: 20,
-                                              foregroundImage: NetworkImage(
-                                                  'https://pbs.twimg.com/profile_images/1488749186610728960/4POimDrS_400x400.jpg'),
-                                            ),
-                                          ),
-                                          const Spacer(),
-                                          Container(
-                                            height: 25,
-                                            width: 80,
-                                            color: CustomColor.primaryBg,
-                                            child: const Center(
-                                                child: Text(
-                                              '200%',
-                                              style: TextStyle(
-                                                color: CustomColor.primaryText,
+                                return GestureDetector(
+                                  onLongPress: () {
+                                    addEditTask(
+                                        context: context,
+                                        type: 'Update',
+                                        docId: '2022-08-01T20:35:52.342559');
+                                  },
+                                  child: Container(
+                                    margin: const EdgeInsets.all(10),
+                                    padding: const EdgeInsets.all(15),
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: CustomColor.cardBg,
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                              child: const CircleAvatar(
+                                                backgroundColor: Colors.amber,
+                                                radius: 20,
+                                                foregroundImage: NetworkImage(
+                                                    'https://pbs.twimg.com/profile_images/1488749186610728960/4POimDrS_400x400.jpg'),
                                               ),
-                                            )),
-                                          ),
-                                        ],
-                                      ),
-                                      const Spacer(),
-                                      Container(
-                                        height: 25,
-                                        width: 100,
-                                        color: CustomColor.primaryBg,
-                                        child: const Center(
-                                            child: Text(
-                                          '20/10 Task',
+                                            ),
+                                            ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                              child: const CircleAvatar(
+                                                backgroundColor: Colors.amber,
+                                                radius: 20,
+                                                foregroundImage: NetworkImage(
+                                                    'https://pbs.twimg.com/profile_images/1488749186610728960/4POimDrS_400x400.jpg'),
+                                              ),
+                                            ),
+                                            ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                              child: const CircleAvatar(
+                                                backgroundColor: Colors.amber,
+                                                radius: 20,
+                                                foregroundImage: NetworkImage(
+                                                    'https://pbs.twimg.com/profile_images/1488749186610728960/4POimDrS_400x400.jpg'),
+                                              ),
+                                            ),
+                                            const Spacer(),
+                                            Container(
+                                              height: 25,
+                                              width: 80,
+                                              color: CustomColor.primaryBg,
+                                              child: const Center(
+                                                  child: Text(
+                                                '200%',
+                                                style: TextStyle(
+                                                  color:
+                                                      CustomColor.primaryText,
+                                                ),
+                                              )),
+                                            ),
+                                          ],
+                                        ),
+                                        const Spacer(),
+                                        Container(
+                                          height: 25,
+                                          width: 100,
+                                          color: CustomColor.primaryBg,
+                                          child: const Center(
+                                              child: Text(
+                                            '20/10 Task',
+                                            style: TextStyle(
+                                              color: CustomColor.primaryText,
+                                            ),
+                                          )),
+                                        ),
+                                        const Text(
+                                          'Pemrograman Mobile (Flutter)',
                                           style: TextStyle(
-                                            color: CustomColor.primaryText,
-                                          ),
-                                        )),
-                                      ),
-                                      const Text(
-                                        'Pemrograman Mobile (Flutter)',
-                                        style: TextStyle(
-                                            color: CustomColor.primaryText,
-                                            fontSize: 20),
-                                      ),
-                                      const Text(
-                                        'Tersisa 3 Hari lagi',
-                                        style: TextStyle(
-                                            color: CustomColor.primaryText,
-                                            fontSize: 15),
-                                      ),
-                                    ],
+                                              color: CustomColor.primaryText,
+                                              fontSize: 20),
+                                        ),
+                                        const Text(
+                                          'Tersisa 3 Hari lagi',
+                                          style: TextStyle(
+                                              color: CustomColor.primaryText,
+                                              fontSize: 15),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 );
                               },
@@ -228,20 +238,122 @@ class TaskView extends GetView<TaskController> {
         alignment: const Alignment(0.9, 0.95),
         child: FloatingActionButton.extended(
           onPressed: () {
-            Get.bottomSheet(Container(
-                margin: !context.isPhone
-                    ? const EdgeInsets.only(right: 150, left: 150)
-                    : null,
-                height: Get.height,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topRight: const Radius.circular(30),
-                      topLeft: Radius.circular(30)),
-                )));
+            addEditTask(context: context, type: 'Add', docId: '');
           },
           label: const Text("Add Task"),
           icon: const Icon(Ionicons.add),
+        ),
+      ),
+    );
+  }
+
+  addEditTask({BuildContext? context, String? type, String? docId}) {
+    Get.bottomSheet(
+      Container(
+        padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+        margin: !context!.isPhone
+            ? const EdgeInsets.only(right: 150, left: 150)
+            : null,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topRight: const Radius.circular(30),
+              topLeft: Radius.circular(30)),
+        ),
+        child: Form(
+          key: controller.formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  '$type Task',
+                  style: const TextStyle(
+                    color: CustomColor.primaryText,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'title',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  controller: controller.titleController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Can not be empty';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    hintText: 'Description',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  controller: controller.descriptionController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Can not be empty';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                DateTimePicker(
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(2100),
+                  dateLabelText: 'Due Date',
+                  decoration: InputDecoration(
+                    hintText: 'Due Date',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  controller: controller.dueDateController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Can not be empty';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ConstrainedBox(
+                  constraints:
+                      BoxConstraints.tightFor(width: Get.width, height: 40),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      controller.saveUpdateTask(
+                        type: type,
+                        titel: controller.titleController.text,
+                        description: controller.descriptionController.text,
+                        dueDate: controller.dueDateController.text,
+                        docId: docId,
+                      );
+                    },
+                    child: Text(type!),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
